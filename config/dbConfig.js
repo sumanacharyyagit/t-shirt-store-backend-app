bd4a5523a016c700465e49f19f6156b44f6bee6c
sumanacharyyagit <sumanacharyya999@gmail.com>
2023-02-18 00:56:17 +0530

    Solved circular dependancy error

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -2,19 +2,19 @@ const mongoose = require("mongoose");
 
 mongoose.set("strictQuery", false);
 const connectWithDB = (uri) => {
-    return mongoose
-        .connect(uri, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        })
-        .then(() => {
-            console.log("MongoDB Connected...!");
-        })
-        .catch((err) => {
-            console.log("DB Connection Failed...!");
-            console.log(err);
-            process.exit(1);
-        });
+  return mongoose
+    .connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log("MongoDB Connected...!");
+    })
+    .catch((err) => {
+      console.log("DB Connection Failed...!");
+      console.log(err);
+      process.exit(1);
+    });
 };
 
 module.exports = connectWithDB;
--- utils/whereClause.js
+++ utils/whereClause.js
@@ -1,58 +1,56 @@
-const { search } = require("../routes/product");
-
 // base --> Product.find({})
 // base --> Product.find({eamil: "suman@suman-dev.in"})
 // bigQ --> ?search=coder&page=2&category=shortSleeves&rating[gte]=4&price[lte]=999&price[gte]=199&limit=5
 
 class WhereClause {
-    constructor(base, bigQ) {
-        this.base = base; // Product model
-        this.bigQ = bigQ; // req.params
-    }
+  constructor(base, bigQ) {
+    this.base = base; // Product model
+    this.bigQ = bigQ; // req.params
+  }
 
-    search() {
-        const searchword = this.bigQ?.search
-            ? {
-                  name: {
-                      $regex: this.bigQ.search,
-                      $options: "i",
-                  },
-              }
-            : {};
-
-        this.base = this.base.find({ ...searchword });
-        return this;
-    }
-
-    filter() {
-        const copyQ = { ...this.bigQ };
-        delete copyQ["search"];
-        delete copyQ["page"];
-        delete copyQ["limit"];
-
-        // convert JSON to String to insert Aggregation
-        let stringOfCopyQ = JSON.stringify(copyQ);
-        stringOfCopyQ = stringOfCopyQ.replace(
-            /\b(gte|lte|gt|lt)\b/g,
-            (m) => `$${m}`
-        );
-        const jsonOfCopyQ = JSON.parse(stringOfCopyQ);
-
-        this.base = this.base.find(jsonOfCopyQ);
-        return this;
-    }
-
-    pager(resultPerPage) {
-        let currentPage = 1;
-        if (this.bigQ?.page) {
-            currentPage = this.bigQ.page;
+  search() {
+    const searchword = this.bigQ?.search
+      ? {
+          name: {
+            $regex: this.bigQ.search,
+            $options: "i",
+          },
         }
+      : {};
 
-        const skipVal = resultPerPage * (currentPage - 1);
+    this.base = this.base.find({ ...searchword });
+    return this;
+  }
 
-        this.base = this.base.limit(resultPerPage).skip(skipVal);
-        return this;
+  filter() {
+    const copyQ = { ...this.bigQ };
+    delete copyQ["search"];
+    delete copyQ["page"];
+    delete copyQ["limit"];
+
+    // convert JSON to String to insert Aggregation
+    let stringOfCopyQ = JSON.stringify(copyQ);
+    stringOfCopyQ = stringOfCopyQ.replace(
+      /\b(gte|lte|gt|lt)\b/g,
+      (m) => `$${m}`
+    );
+    const jsonOfCopyQ = JSON.parse(stringOfCopyQ);
+
+    this.base = this.base.find(jsonOfCopyQ);
+    return this;
+  }
+
+  pager(resultPerPage) {
+    let currentPage = 1;
+    if (this.bigQ?.page) {
+      currentPage = this.bigQ.page;
     }
+
+    const skipVal = resultPerPage * (currentPage - 1);
+
+    this.base = this.base.limit(resultPerPage).skip(skipVal);
+    return this;
+  }
 }
 
 module.exports = WhereClause;
